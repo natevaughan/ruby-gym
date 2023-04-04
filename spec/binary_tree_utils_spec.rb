002d376d817b47a44e0394474abc20dbc049708c
Nate Vaughan <nate.vaughan@protonmail.com>
2023-04-03 21:40:44 -0500

    Working toward AVL balancing rotations

--- spec/binary_tree_utils_spec.rb
+++ spec/binary_tree_utils_spec.rb
@@ -20,21 +20,43 @@ describe BinaryTreeUtils do
 
   describe "#build" do
     it "builds a tree of depth 1" do
-      root_node = BinaryTreeUtils::construct([1])
-      expect(root_node.value).to eq(1)
-      expect(root_node.left).to be_nil
-      expect(root_node.right).to be_nil
+      tree = BinaryTree.new([1])
+      expect(tree.root.value).to eq(1)
+      expect(tree.root.left).to be_nil
+      expect(tree.root.right).to be_nil
     end
 
     it "builds a tree of depth 2" do
-      root_node = BinaryTreeUtils::construct([2, 1, 4])
-      expect(root_node.value).to eq(2)
-      expect(root_node.left.value).to eq(1)
-      expect(root_node.right.value).to eq(4)
+      tree = BinaryTree.new([2, 1, 4])
+      expect(tree.root.value).to eq(2)
+      expect(tree.root.left.value).to eq(1)
+      expect(tree.root.right.value).to eq(4)
     end
 
-    it "expects argument to be a TreeNode" do
-      expect { BinaryTreeUtils::construct("illegal argument") }.to raise_error(IllegalArgumentException)
+    it "builds a balanced tree" do
+      tree = BinaryTree.new([1, 2, 3, 4])
+      expect(BinaryTreeUtils::find_depth(tree.root)).to eq(4) # todo
+    end
+
+    it "expects argument to be an Array" do
+      expect { BinaryTree.new("illegal argument") }.to raise_error(IllegalArgumentException)
+    end
+  end
+
+  describe "#find_depth" do
+    it "returns 1 for a single node tree" do
+      tree = BinaryTree.new([1])
+      expect(BinaryTreeUtils::find_depth(tree.root)).to eq(1)
+    end
+
+    it "returns 2 for a tree of depth 2" do
+      tree = BinaryTree.new([2, 1, 4])
+      expect(BinaryTreeUtils::find_depth(tree.root)).to eq(2)
+    end
+
+    it "returns 3 for a balanced tree of depth 3" do
+      tree = BinaryTree.new([5,3, 2, 4, 9, 7, 100])
+      expect(BinaryTreeUtils::find_depth(tree.root)).to eq(3)
     end
   end
 end
--- workouts/binary_tree_utils.rb
+++ workouts/binary_tree_utils.rb
@@ -12,6 +12,44 @@ class BinaryNode
   end
 end
 
+# AVL balanced binary tree
+class BinaryTree
+  attr_reader :root
+
+  def initialize(arr)
+    @root = nil
+    return if arr == nil
+    raise IllegalArgumentException.new("Can only create a binary tree from an array") unless arr&.is_a? Array
+    arr.each { | el | add(el) }
+  end
+
+  def add(value)
+    if @root == nil
+      @root = BinaryNode.new(value)
+    else
+        insert(value, @root)
+    end
+  end
+
+  def insert(value, node)
+    return unless node != nil
+
+    if value < node.value
+      if node.left == nil
+        node.left = BinaryNode.new(value)
+      else
+        insert(value, node.left)
+      end
+    else
+      if node.right == nil
+        node.right = BinaryNode.new(value)
+      else
+        insert(value, node.right)
+      end
+    end
+  end
+end
+
 module BinaryTreeUtils
   def self.traverse(node, arr = [])
      raise IllegalArgumentException.new("Can only traverse a binary node") unless node.is_a? BinaryNode
@@ -25,32 +63,12 @@ module BinaryTreeUtils
      return arr
   end
 
-  def self.construct(arr)
-    raise IllegalArgumentException.new("Can only build a binary tree from an array") unless arr.is_a? Array
-    root_node = nil
-    arr.each do |el|
-      if root_node == nil
-        root_node = BinaryNode.new(el)
-      else
-        insert_node(el, root_node)
-      end
-    end
-    return root_node
-  end
-
-  def self.insert_node(value, node)
-    if value < node.value
-      if node.left == nil
-        node.left = BinaryNode.new(value)
-      else
-        insert_node(value, node.left)
-      end
+  def self.find_depth(node, depth = 1)
+    return depth - 1 if node == nil
+    if node.left == nil && node.right == nil
+      return depth
     else
-      if node.right == nil
-        node.right = BinaryNode.new(value)
-      else
-        insert_node(value, node.right)
-      end
+      return [find_depth(node.left, depth + 1), find_depth(node.right, depth + 1)].max
     end
   end
 end
